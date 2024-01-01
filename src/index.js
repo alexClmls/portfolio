fc368a7ff125d581679c8731ebbc7ae2ca0cfcc9
Alexandre Calmels <acalmels92@hotmail.fr>
2024-01-01 02:57:33 +0100

    new portfolio

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client';
-import "./index.css"
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './components/AppRouter/appRouter';
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-        <AppRouter /> 
-    </Router>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
+
--- src/pages/Contact/contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact() {
-  return (
-    <div>
-      <h2>Contactez-moi</h2>
-    </div>
-  );
-}
-
-export default Contact;
